5fdd82ffd484138cc9ee543dbf462ec00537fc1f
SandeshGC <gcsandesh01@gmail.com>
2023-01-03 22:25:12 +0545

    create basic navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
-	return <div className="App font-semibold">PhotoGalaxy</div>;
+	return (
+		<div className="App">
+			<Header />
+			
+		</div>
+	);
 }
 
 export default App;
