f27100e721bbfb0b3d71054ab8189b8f0ae0f11a
SandeshGC <gcsandesh01@gmail.com>
2023-01-04 22:49:20 +0545

    complete header design

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import { SiteLogo } from "../common";
+import { FaChevronDown, FaRegBell, FaRegUserCircle } from "react-icons/fa";
 
 export default function Header() {
 	return (
-		<div className="bg-gradient-to-b from-grey-900 to-grey-700 shadow-md p-4">
+		<div className="bg-gradient-to-b from-grey-900 to-grey-700 shadow-md p-4 mb-4">
 			<div className="container mx-auto flex items-center justify-between ">
 				<SiteLogo isDark={true} />
 				<SearchBar />
+				<div className="flex gap-4">
+					<div className="flex gap-4">
+						<div className="flex items-center gap-1">
+							<span>Explore</span> <FaChevronDown size={12} />
+						</div>
+						<span>Upload</span>
+					</div>
+					<FaRegBell size={24} />
+					<FaRegUserCircle size={24} />
+				</div>
 			</div>
 		</div>
 	);
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Home() {
-	return <div className="Home">HOME PAGE</div>;
+	return (
+		<div className="Home">
+			<div className="container mx-auto">HOME PAGE</div>
+		</div>
+	);
 }
