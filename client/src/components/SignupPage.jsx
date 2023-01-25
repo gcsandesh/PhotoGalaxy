dea4f4e302ea75aa643a2f8ac052dc2e129ccf04
subek sharma <subeksharmaofficial@gmail.com>
2023-01-25 18:07:21 +0545

    updated sign up page

---
+++ client/src/components/SignupPage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function SignupPage() {
+	return (
+		<div>
+			<label>Email: </label>
+			<input type="text" placeholder="Enter Your Email" name="email" required />
+			<label>Password : </label>
+			<input type="password" placeholder="Enter Your Password" name="password" required />
+			<label>Confirm Password : </label>
+			<input type="password" placeholder="Enter Your Password Again" name="password" required />
+			<button type="submit">Sign Up</button>
+			Already have an account?
+			<a href="#"> Log In  </a>
+			
+		</div>
+	); 
+}
+
+
