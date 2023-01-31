c4818f38e35b5b356858d4e81251891c36ca7fce
tsubek <subeksharmaofficial@gamil.com>
2023-01-31 15:25:40 +0545

    sign up page styled with bootstrap

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -3,25 +3,25 @@ import React from "react"
 export default function Signup() {
 	return (
 		<div>
-			<label>Email: </label>
-			<input type="text" placeholder="Enter Your Email" name="email" required />
-			<label>Password : </label>
-			<input
-				type="password"
-				placeholder="Enter Your Password"
-				name="password"
-				required
-			/>
-			<label>Confirm Your Password : </label>
-			<input
-				type="password"
-				placeholder="Enter Your Password Again"
-				name="password"
-				required
-			/>
-			<button type="submit">Sign Up</button>
-			Already have an account ?
-			<a href="#"> Log In </a>
+			<div class="container">
+				<form>
+					<div class="form-group">
+						<label>Email: </label>
+						<input type="text" class="form-control" placeholder="Enter Your Email" name="email" required />
+					</div>
+					<div class="form-group">
+						<label>Password : </label>
+						<input type="password" class="form-control" placeholder="Enter Your Password" name="password" required />
+					</div>
+					<div class="form-group">
+						<label>Confirm Your Password : </label>
+						<input type="password" class="form-control" placeholder="Enter Your Password Again" name="password" required />
+					</div>
+					<button type="submit" class="btn btn-primary">Sign Up</button>
+					<p>Already have an account ? <a href="#"> Log In </a></p>
+				</form>
+			</div>
+
 		</div>
 	)
 }
