4432204c79f53f7d088271c6701bdc6b36150fd1
tsubek <subeksharmaofficial@gamil.com>
2023-02-01 21:13:48 +0545

    passworg changing pade styled

--- client/src/pages/ChangePassword.jsx
+++ client/src/pages/ChangePassword.jsx
@@ -3,19 +3,25 @@ import React from "react"
 export default function ChangePassword() {
 	return (
 		<div>
-			<h1>Change Password</h1>
-			<form action="/change-password" method="post" />
-			<label>Current Password:</label>
-			<input type="password" id="current-password" name="current-password" />
-			<br />
-			<label for="new-password">New Password:</label>
-			<input type="password" id="new-password" name="new-password" />
-			<br />
-			<label for="confirm-password">Confirm Password:</label>
-			<input type="password" id="confirm-password" name="confirm-password" />
-			<br />
-			<input type="submit" value="Change Password" />
-			<form />
+			<h1 class="text-center">Change Password</h1>
+			<div class="container">
+				<form action="/change-password" method="post">
+					<div class="form-group">
+						<label for="current-password">Current Password:</label>
+						<input type="password" class="form-control" id="current-password" name="current-password"/>
+					</div>
+					<div class="form-group">
+						<label for="new-password">New Password:</label>
+						<input type="password" class="form-control" id="new-password" name="new-password"/>
+					</div>
+					<div class="form-group">
+						<label for="confirm-password">Confirm Password:</label>
+						<input type="password" class="form-control" id="confirm-password" name="confirm-password"/>
+					</div>
+					<input type="submit" class="btn btn-primary" value="Change Password"/>
+				</form>
+			</div>
+
 		</div>
 	)
 }
