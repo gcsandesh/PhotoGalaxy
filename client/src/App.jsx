6ef09f85d0469ce09c1f0fff32a2244d1e330d85
subeksharma <subeksharmaofficial@gmail.com>
2023-02-02 15:00:15 +0545

    some minor changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, BrowserRouter as Router } from "react-router-dom"
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom" 
 import "./App.css"
 import { Header } from "./components"
 import { Footer } from "./components/"
