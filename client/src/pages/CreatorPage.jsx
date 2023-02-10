8e3eedbfbb44f295b58437420fe02bb4ea158f64
subeksharma <subeksharmaofficial@gmail.com>
2023-02-10 09:35:34 +0545

    minor change in the syntax of componrnts

--- client/src/pages/CreatorPage.jsx
+++ client/src/pages/CreatorPage.jsx
@@ -3,22 +3,22 @@ import React from "react"
 export default function CreatorPage() {
     return (
         <div>
-            <div class="container">
-                <img class="profile-pic" src="path/to/profile_pic.jpg" alt="Profile Picture Of The Creator" />
+            <div className="container">
+                <img className="profile-pic" src="path/to/profile_pic.jpg" alt="Profile Picture Of The Creator" />
             </div>
-            <div class="container">
+            <div className="container">
                 <h1> Subek Adhikary  </h1>
-                <div class="bio">
+                <div className="bio">
                     <p>Hello, i love clicking pictures. This is my hobby.</p>
                 </div>
                 <h2> 5000 Points </h2>
             </div>
-            <div class="container">
+            <div className="container">
                 <h3>Photos by Subek Adhikary </h3>
-                <div class="photos" />
-                <img class="photo" src="path/to/photo1.jpg" alt="Photo 1" />
-                <img class="photo" src="path/to/photo2.jpg" alt="Photo 2" />
-                <img class="photo" src="path/to/photo3.jpg" alt="Photo 3" />
+                <div className="photos" />
+                <img className="photo" src="path/to/photo1.jpg" alt="Photo 1" />
+                <img className="photo" src="path/to/photo2.jpg" alt="Photo 2" />
+                <img className="photo" src="path/to/photo3.jpg" alt="Photo 3" />
             </div>
         </div>
 
