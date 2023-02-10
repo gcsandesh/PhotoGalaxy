6f9601b09137cbeb0cb76cb984d5a4050a473886
subeksharma <subeksharmaofficial@gmail.com>
2023-02-10 09:39:05 +0545

    class changed to classname in the component

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,21 +3,21 @@ import React from "react"
 export default function Login() {
 	return (
 		<div>
-			<div class="container mt-5">
-				<h2 class="text-center mb-5">Login</h2>
+			<div className="container mt-5">
+				<h2 className="text-center mb-5">Login</h2>
 				<form>
-					<div class="form-group">
+					<div className="form-group">
 						<label for="email">Email:</label>
-						<input type="email" class="form-control" id="email" placeholder="Enter email" required/>
+						<input type="email" className="form-control" id="email" placeholder="Enter email" required/>
 					</div>
-					<div class="form-group">
+					<div className="form-group">
 						<label for="password">Password:</label>
-						<input type="password" class="form-control" id="password" placeholder="Enter password" required/>
+						<input type="password" className="form-control" id="password" placeholder="Enter password" required/>
 					</div>
-					<button type="submit" class="btn btn-primary btn-block">Login</button>
-					<a class="float-right mt-2" href="#">Forgot password?</a>
+					<button type="submit" className="btn btn-primary btn-block">Login</button>
+					<a className="float-right mt-2" href="#">Forgot password?</a>
 				</form>
-				<div class="text-center mt-3">
+				<div className="text-center mt-3">
 					Don't have an account? <a href="#">Sign up</a>
 				</div>
 			</div>
