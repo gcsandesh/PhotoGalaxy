563ec421eb0b73e79f47292391cbfe55f60415be
subeksharma <subeksharmaofficial@gmail.com>
2023-02-10 09:57:48 +0545

    class changed to classname

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -3,21 +3,21 @@ import React from "react"
 export default function Signup() {
 	return (
 		<div>
-			<div class="container">
+			<div className="container">
 				<form>
-					<div class="form-group">
+					<div className="form-group">
 						<label>Email: </label>
-						<input type="text" class="form-control" placeholder="Enter Your Email" name="email" required />
+						<input type="text" className="form-control" placeholder="Enter Your Email" name="email" required />
 					</div>
-					<div class="form-group">
+					<div className="form-group">
 						<label>Password : </label>
-						<input type="password" class="form-control" placeholder="Enter Your Password" name="password" required />
+						<input type="password" className="form-control" placeholder="Enter Your Password" name="password" required />
 					</div>
-					<div class="form-group">
+					<div className="form-group">
 						<label>Confirm Your Password : </label>
-						<input type="password" class="form-control" placeholder="Enter Your Password Again" name="password" required />
+						<input type="password" className="form-control" placeholder="Enter Your Password Again" name="password" required />
 					</div>
-					<button type="submit" class="btn btn-primary">Sign Up</button>
+					<button type="submit" className="btn btn-primary">Sign Up</button>
 					<p>Already have an account ? <a href="#"> Log In </a></p>
 				</form>
 			</div>
