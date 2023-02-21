d13f8b2aa7849ebc5d69e176ad48e815fc2b0f21
SandeshGC <gcsandesh01@gmail.com>
2023-02-21 14:23:17 +0545

    fix creator page error

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,30 +1,33 @@
 import React from "react"
-import { SiteLogo } from "../common"
+import { Link } from "react-router-dom"
 import { FaRegBell, FaRegUserCircle } from "react-icons/fa"
 import { Container, Nav, NavItem, NavDropdown, Button } from "react-bootstrap"
 import DropdownItem from "react-bootstrap/esm/DropdownItem"
+import { SiteLogo } from "../common"
 
 export default function Header() {
-	return (
-		<Container fluid className="shadow mb-4">
-			<Container className="d-flex justify-content-between">
-				<SiteLogo />
+  return (
+    <Container fluid className="shadow mb-4">
+      <Container className="d-flex justify-content-between">
+        <Link to="/">
+          <SiteLogo />
+        </Link>
 
-				<Nav className="d-flex gap-2 align-items-center">
-					<NavDropdown title="Explore">
-						<DropdownItem>Top Downloaded</DropdownItem>
-					</NavDropdown>
-					<NavItem>
-						<Button>Upload</Button>
-					</NavItem>
-					<NavItem>
-						<FaRegBell size={24} />
-					</NavItem>
-					<NavItem>
-						<FaRegUserCircle size={24} />
-					</NavItem>
-				</Nav>
-			</Container>
-		</Container>
-	)
+        <Nav className="d-flex gap-2 align-items-center">
+          <NavDropdown title="Explore">
+            <DropdownItem>Top Downloaded</DropdownItem>
+          </NavDropdown>
+          <NavItem>
+            <Button>Upload</Button>
+          </NavItem>
+          <NavItem>
+            <FaRegBell size={24} />
+          </NavItem>
+          <NavItem>
+            <FaRegUserCircle size={24} />
+          </NavItem>
+        </Nav>
+      </Container>
+    </Container>
+  )
 }
