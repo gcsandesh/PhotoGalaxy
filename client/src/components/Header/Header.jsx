afccb5713fcee4e169ab625cea1d0ce749bc71e5
SandeshGC <gcsandesh01@gmail.com>
2023-02-21 15:58:09 +0545

    add dummy notifications

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "react-router-dom"
 import { FaRegBell, FaRegUserCircle } from "react-icons/fa"
 import { Container, Nav, NavItem, NavDropdown, Button } from "react-bootstrap"
@@ -6,9 +6,41 @@ import DropdownItem from "react-bootstrap/esm/DropdownItem"
 import { SiteLogo } from "../common"
 
 export default function Header() {
+  // const [isNotificationOn, setIsNotificationOn] = useState(false)
+  const [notifications, setNotifications] = useState([
+    {
+      notificationID: "somerandomid1",
+      date: "2023-02-21",
+      time: "15:45:00",
+      message: "Congratulations! You have received 10 points.",
+    },
+    {
+      notificationID: "somerandomid2",
+      date: "2023-02-21",
+      time: "15:45:00",
+      message: "John started following you.",
+    },
+    {
+      notificationID: "somerandomid3",
+      date: "2023-02-21",
+      time: "15:45:00",
+      message: "Upload complete!",
+    },
+  ])
+
+  const notificationEls = notifications.map((notif) => (
+    <DropdownItem key={notif.notificationID} className="border">
+      {notif.message}
+    </DropdownItem>
+  ))
+
   function toggleNotification() {
-    console.log("on/off")
+    // here we should fetch notifications for that user
+    //setNotification("the fetched notification")
+    // setIsNotificationOn((prevState) => !prevState)
   }
+
+  // console.log(isNotificationOn)
   return (
     <Container fluid className="shadow mb-4">
       <Container className="d-flex justify-content-between">
@@ -24,7 +56,9 @@ export default function Header() {
             </Link>
           </NavItem>
           <NavItem onClick={toggleNotification}>
-            <FaRegBell size={24} />
+            <NavDropdown title={<FaRegBell size={24} />}>
+              {notificationEls}
+            </NavDropdown>
           </NavItem>
           <NavItem>
             <FaRegUserCircle size={24} />
--- client/src/components/common/SiteLogo.jsx
+++ client/src/components/common/SiteLogo.jsx
@@ -4,9 +4,9 @@ import Image from "react-bootstrap/Image"
 import img from "../../assets/logo.svg"
 
 export default function SiteLogo() {
-	return (
-		<Link to="/">
-			<Image src={img} width={200} height={45} />
-		</Link>
-	)
+  return (
+    <Link to="/">
+      <Image src={img} width={200} height={45} />
+    </Link>
+  )
 }
