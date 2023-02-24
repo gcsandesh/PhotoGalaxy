06d8cb034a1f883f5c34ddddf28643d3e90708a4
SandeshGC <gcsandesh01@gmail.com>
2023-02-24 23:57:33 +0545

    add .env.sample and upload widget from cloudinary

---
+++ client/src/components/UploadWidget.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function UploadWidget() {
+  return (
+    <div>UploadWidget</div>
+  )
+}
--- client/src/pages/PhotoUploadPage.jsx
+++ client/src/pages/PhotoUploadPage.jsx
@@ -1,11 +1,62 @@
 import React from "react"
 
 export default function PhotoUploadPage() {
+  function showUploadWidget() {
+    cloudinary.openUploadWidget(
+      {
+        cloudName: "<cloud name>",
+        uploadPreset: "<upload preset>",
+        sources: [
+          "local",
+          "url",
+          "camera",
+          "image_search",
+          "google_drive",
+          "facebook",
+          "dropbox",
+          "instagram",
+          "shutterstock",
+          "getty",
+          "istock",
+          "unsplash",
+        ],
+        googleApiKey: "<image_search_google_api_key>",
+        showAdvancedOptions: true,
+        cropping: true,
+        multiple: false,
+        defaultSource: "local",
+        styles: {
+          palette: {
+            window: "#FFFFFF",
+            windowBorder: "#90A0B3",
+            tabIcon: "#0078FF",
+            menuIcons: "#5A616A",
+            textDark: "#000000",
+            textLight: "#FFFFFF",
+            link: "#0078FF",
+            action: "#FF620C",
+            inactiveTabIcon: "#0E2F5A",
+            error: "#F44235",
+            inProgress: "#0078FF",
+            complete: "#20B832",
+            sourceBg: "#E4EBF1",
+          },
+          fonts: { default: { active: true } },
+        },
+      },
+      (err, info) => {
+        if (!err) {
+          console.log("Upload Widget event - ", info)
+        }
+      }
+    )
+  }
   return (
     <div>
       <div className="container">
         <h1 className="text-center my-5">Upload Your Photos</h1>
-        <form
+        <button onClick={showUploadWidget}>Upload</button>
+        {/* <form
           action="/upload-photo"
           method="post"
           encType="multipart/form-data"
@@ -46,7 +97,7 @@ export default function PhotoUploadPage() {
             />
           </div>
           <input type="submit" className="btn btn-primary" value="Upload" />
-        </form>
+        </form> */}
       </div>
     </div>
   )
