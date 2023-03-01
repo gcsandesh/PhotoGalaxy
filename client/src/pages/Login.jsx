425ad2b708441facbef2adcc73bce3a40fc10ea8
subeksharma <subeksharmaofficial@gmail.com>
2023-03-01 21:32:58 +0545

    bootstrap removed from login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,37 +3,35 @@ import { Link } from "react-router-dom"
 
 export default function Login() {
   return (
-    <div className="container mt-5">
-      <h2 className="text-center mb-5">Login</h2>
+    <div >
+      <h2 >Login</h2>
       <form>
-        <div className="form-group">
+        <div >
           <label htmlFor="email">Email:</label>
           <input
             type="email"
-            className="form-control"
             id="email"
             placeholder="Enter email"
             required
           />
         </div>
-        <div className="form-group">
+        <div >
           <label htmlFor="password">Password:</label>
           <input
             type="password"
-            className="form-control"
             id="password"
             placeholder="Enter password"
             required
           />
         </div>
-        <button type="submit" className="btn btn-primary btn-block">
+        <button type="submit" >
           Login
         </button>
-        <Link className="float-right mt-2" to={"/reset-password"}>
+        <Link to={"/reset-password"}>
           Forgot password?
         </Link>
       </form>
-      <div className="text-center mt-3">
+      <div >
         Don't have an account? <Link to="/signup">Sign up</Link>
       </div>
     </div>
