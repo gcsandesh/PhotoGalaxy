f74156f594ab454597b0591b01b073960c749676
SandeshGC <gcsandesh01@gmail.com>
2023-03-02 16:10:44 +0545

    implement cloudinary

--- server/src/controllers/photos.js
+++ server/src/controllers/photos.js
@@ -1,32 +1,41 @@
+const cloudinary = require('cloudinary').v2
+
 ////////////    UPLOAD ONE OR MANY PHOTOS    /////////////
 function uploadPhotos(req, res) {
-  console.log("files", req.files)
+  console.log('files', req.files)
   console.log(req.body)
-  console.log("query", req.query)
-  console.log("photo is received and uploaded to server")
+  console.log('query', req.query)
+  console.log('photo is received and uploaded to server')
+
+  const resp = cloudinary.uploader.upload(req.files[0], {
+    public_id: 'first_upload_gc',
+  })
+
+  resp.then(() => console.log('uploaded')).catch((err) => console.log(err))
+
   return res.send({ message: req.body })
 }
 
 ////////////    GET PHOTO    /////////////
 function getPhoto() {
-  console.log("a photo is sent")
+  console.log('a photo is sent')
   return
 }
 
 ////////////    GET ALL PHOTOS    /////////////
 function getAllPhotos() {
-  console.log("All photos are sent!")
+  console.log('All photos are sent!')
 }
 
 ////////////    GET ALL PHOTOS FROM A CATEGORY    /////////////
 function getPhotosFromCategory(req, res) {
-  console.log("Send photos of specific category")
+  console.log('Send photos of specific category')
 }
 
 ////////////    GET PHOTOS SIMILAR TO A PHOTO    /////////////
 function getSimilarPhotos(req, res) {
   // get a photo from request and send other photos that are similar to them
-  console.log("similar photos are sent")
+  console.log('similar photos are sent')
 }
 
 module.exports = {
