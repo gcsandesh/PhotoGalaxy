d4875982002609efe6578f3d7b2afdff3edde566
subeksharma <subeksharmaofficial@gmail.com>
2023-03-09 12:32:13 +0545

    similar recommendor model added

---
+++ server/similar-recommendor/predict.js
@@ -0,0 +1,18 @@
+const spawn = require("child_process").spawn;
+
+let image_path='https://cdn.britannica.com/55/174255-050-526314B6/brown-Guernsey-cow.jpg?w=400&h=300&c=crop'
+let number_of_images=10
+
+const pythonProcess = spawn("python.exe", ["./predict.py", image_path,number_of_images]);
+
+pythonProcess.stdout.on("data", (data) => {
+  console.log(data.toString());
+});
+
+pythonProcess.stderr.on("data", (data) => {
+  console.error("error", data.toString());
+});
+
+pythonProcess.on("close", (code) => {
+  console.log("process exited with code: ", code);
+});
---
+++ server/similar-recommendor/predict.py
@@ -0,0 +1,22 @@
+import sys
+import requests
+from io import BytesIO
+from DeepImageSearch import SearchImage
+
+
+
+# Send a GET request to the image URL
+response = requests.get(sys.argv[1])
+
+# Get the image data from the response
+image_data = response.content
+
+
+# Store the image data as a variable
+image_variable = BytesIO(image_data)
+
+
+
+print(SearchImage().get_similar_images(image_variable, int(sys.argv[2])))
+
+
