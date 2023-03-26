d716fa004f0404213fdcd4471812c5be487e1450
SandeshGC <gcsandesh01@gmail.com>
2023-03-26 20:42:08 +0545

    add * to header access control allow origin

--- server/src/config/app.js
+++ server/src/config/app.js
@@ -18,10 +18,7 @@ app.use(cors({ origin: "https://photogalaxy-client.vercel.app" }))
 // Add Access Control Allow Origin headers
 
 app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://photogalaxy-client.vercel.app"
-  )
+  res.setHeader("Access-Control-Allow-Origin", "*")
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
