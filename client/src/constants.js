7381c26a0c0eea514bbfe4ef754020d6c5afd82e
SandeshGC <gcsandesh01@gmail.com>
2023-03-26 20:53:17 +0545

    modify photo upload url

--- client/src/constants.js
+++ client/src/constants.js
@@ -9,7 +9,7 @@ export const GET_PHOTO_BY_ID = API_URL + "/photos/id/"
 export const GET_USER_UPLOADS = API_URL + "/photos?uploaded_by="
 export const GET_USER_LIKES = API_URL + "/photos?liked_by="
 export const GET_ALL_PHOTOS = API_URL + "/photos"
-export const UPLOAD_PHOTOS = API_URL + "/photos/"
+export const UPLOAD_PHOTOS = API_URL + "/photos"
 export const DELETE_PHOTO = API_URL + "/photos/id/"
 
 // USER
