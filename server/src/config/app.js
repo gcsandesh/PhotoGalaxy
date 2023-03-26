7767c8130b982f8f5f03c300f21c986db50fcb83
SandeshGC <gcsandesh01@gmail.com>
2023-03-26 11:09:33 +0545

    add frontend origin to cors option

--- server/src/config/app.js
+++ server/src/config/app.js
@@ -12,7 +12,7 @@ require("dotenv").config({ path: "../.env" })
 app.set("view engine", "ejs")
 app.use(xss())
 app.use(helmet())
-app.use(cors())
+app.use(cors({origin: "https://photogalaxy-client.vercel.app/"}))
 app.use(express.json({ limit: "50mb" }))
 
 app.use(express.urlencoded({ extended: true, limit: "50mb" }))
