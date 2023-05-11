9a30fb65b238cc52c6edf99934cacd86be336623
SandeshGC <gcsandesh01@gmail.com>
2023-05-11 09:29:47 +0545

    fix import

--- client/src/pages/Utilities/Protected.jsx
+++ client/src/pages/Utilities/Protected.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { useSelector } from "react-redux"
-import { Navigate } from "react-router"
+import { Navigate } from "react-router-dom"
 
 export default function Protected({ children, admin }) {
   const {
