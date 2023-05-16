99ba29b909feaba196cfea588c45b802e7ab68c5
subeksharma <subeksharmaofficial@gmail.com>
2023-05-16 13:36:26 +0545

    unnecessary comments removed

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 from io import BytesIO
 
 
-model = tf.keras.models.load_model('server\src\cnn_model.h5')
+model = tf.keras.models.load_model('server\\src\\cnn_model.h5') 
 
 app = Flask(__name__, template_folder = 'templates')
 
--- server/similar-recommender/classify.py
+++ server/similar-recommender/classify.py
@@ -14,19 +14,6 @@ image_data = response.content
 # Store the image data as a variable (here we are using PIL object and not Byte object because we cannot manipulate byte object like an image to crop and preprocess )
 image_variable = Image.open(BytesIO(image_data))
 
-# import os
-# execution_path = os.getcwd()
-
-# prediction = ImageClassification()
-# prediction.setModelTypeAsInceptionV3()
-# prediction.setModelPath('C:\\Users\\subek\\Desktop\\python-in-node\\inception_v3_google-1a9a5a14.pth')
-# prediction.loadModel()
-
-# predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, "C:\\Users\\subek\\Desktop\\python-in-node\\img.jpg"), result_count=10)
-# for eachPrediction, eachProbability in zip(predictions, probabilities):
-#     print(eachPrediction , " : " , eachProbability)
-
-
 prediction = ImageClassification()
 # Here i used pretrained inception Inception model however any one could be used
 prediction.setModelTypeAsInceptionV3()
