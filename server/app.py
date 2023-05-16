0363f12f17018d21c4027a2fa6a0e22b8ca3a6d5
subeksharma <subeksharmaofficial@gmail.com>
2023-05-16 16:36:59 +0545

    child process files deleted and replaced with python backend server

--- server/app.py
+++ server/app.py
@@ -1,22 +1,25 @@
-from flask import Flask, request, render_template
+from imageai.Classification import ImageClassification
+from PIL import Image
+from flask import Flask, request, jsonify, render_template
 import numpy as np
 import tensorflow as tf
 from io import BytesIO
 
 
-model = tf.keras.models.load_model('server\\src\\cnn_model.h5') 
+model = tf.keras.models.load_model("server\\src\\cnn_model.h5")
 
 app = Flask(__name__)
 
 
-@app.route('/classify', methods=['POST'])
+@app.route("/classify", methods=["POST"])
 def classify_image():
     # Get the file from the request
-    file = request.files['file']
+    file = request.files["file"]
 
     # Read the file
     img = tf.keras.preprocessing.image.load_img(
-        BytesIO(file.read()), target_size=(240, 320))
+        BytesIO(file.read()), target_size=(240, 320)
+    )
 
     # Convert the image to a numpy array
     img_array = tf.keras.preprocessing.image.img_to_array(img)
@@ -24,35 +27,40 @@ def classify_image():
     # Normalize the image
     img_array /= 255.0
 
-    # Add a batch dimension
-    img_array = np.expand_dims(img_array, axis=0)
-
     # Predict the class
     prediction = model.predict(img_array)
 
     # Get the predicted class label
-    if prediction[0]<0.3 :
-         label = ' 18+ CONTENT.'
+    if prediction[0] < 0.3:
+        label = " 18+ CONTENT."
     else:
-         label = 'SAFE TO USE.'
+        label = "SAFE TO USE."
 
-    # Render the index template with the file upload form and classification result
-    return render_template('index.html', label=label)
+    return jsonify(label)
 
 
-@app.route('/tags', methods=['POST'])
-def handle_tags():
-    # Perform your specific task here
-    # Retrieve the data from the API call
-    data = request.get_json()
-    
-    # Process the data and generate the array of text
-    text_array = ['tag1', 'tag2', 'tag3']  # Replace with your actual logic
-    
+@app.route("/tags", methods=["POST"])
+def generate_tags():
+    # Get the file from the request
+    file = request.files["file"]
+
+    # Read the file
+    img = tf.keras.preprocessing.image.load_img(BytesIO(file.read()))
+
+    prediction = ImageClassification()
+    # Here i used pretrained inception Inception model however any one could be used
+    prediction.setModelTypeAsInceptionV3()
+
+    # Directly give the path where the model is stored or use above code to join paths
+    prediction.setModelPath("server\\similar-recommender\\inception_v3_google.pth")
+    prediction.loadModel()
+
+    # Give path to the image which is to be classified
+    predictions = prediction.classifyImage(img, result_count=5)
+
     # Return the JSON response
-    return jsonify({'tags': text_array})
+    return jsonify(predictions[0])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
-
