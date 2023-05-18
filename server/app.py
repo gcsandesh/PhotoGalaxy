dfa43fb23be5f88ffa8ab4dcb8efaf7ca73c08db
subeksharma <subeksharmaofficial@gmail.com>
2023-05-18 17:41:45 +0545

    similar.js file deleted

--- server/app.py
+++ server/app.py
@@ -1,7 +1,6 @@
 from imageai.Classification import ImageClassification
 from flask import Flask, request, jsonify
 import tensorflow as tf
-import base64
 import io
 from io import BytesIO
 from flask_cors import CORS
@@ -9,8 +8,6 @@ import numpy as np
 import os
 import tensorflow as tf
 
-model_path = os.path.join("server", "src", "cnn_model.h5")
-model = tf.keras.models.load_model(model_path)
 
 app = Flask(__name__)
 CORS(app)
@@ -18,23 +15,13 @@ CORS(app)
 
 @app.route("/classify", methods=["POST"])
 def classify_image():
-    # # Get the Base64-encoded image from the request
-    # image_data = request.json["image"]
-
-    # # Decode the Base64-encoded image to binary
-    # image_bytes = base64.b64decode(image_data)
-
-    # # Load the image from bytes
-    # img = tf.keras.preprocessing.image.load_img(
-    #     io.BytesIO(image_bytes), target_size=(240, 320)
-    # )
-
     # Get the file from the request
-    file = request.files['photo']
+    file = request.files["photo"]
 
     # Read the file
     img = tf.keras.preprocessing.image.load_img(
-        BytesIO(file.read()), target_size=(240, 320))
+        BytesIO(file.read()), target_size=(240, 320)
+    )
 
     # Convert the image to a numpy array
     img_array = tf.keras.preprocessing.image.img_to_array(img)
@@ -45,53 +32,60 @@ def classify_image():
     # Add an additional dimension to match the expected input shape
     img_array = np.expand_dims(img_array, axis=0)
 
+    model_path = os.path.join("server", "src", "cnn_model.h5")
+    model = tf.keras.models.load_model(model_path)
+
     # Predict the class
     prediction = model.predict(img_array)
 
     # Get the predicted class label
     if prediction[0] < 0.3:
-        label = 0 #18+ content
+        label = 0  # 18+ content
     else:
-        label = 1 #safe to use 
+        label = 1  # safe to use
 
     return jsonify(label)
 
 
 @app.route("/tags", methods=["POST"])
 def generate_tags():
-    # # Get the Base64-encoded image from the request
-    # image_data = request.json["image"]
-
-    # # Decode the Base64-encoded image to binary
-    # image_bytes = base64.b64decode(image_data)
-
-    # # Load the image from bytes
-    # img = tf.keras.preprocessing.image.load_img(
-    #     io.BytesIO(image_bytes), target_size=(240, 320)
-    # )
-
     # Get the file from the request
-    file = request.files['file']
+    file = request.files["photo"]
 
     # Read the file
     img = tf.keras.preprocessing.image.load_img(
-        BytesIO(file.read()), target_size=(240, 320))
+        BytesIO(file.read()), target_size=(240, 320)
+    )
 
     prediction = ImageClassification()
+
     # Here i used pretrained inception Inception model however any one could be used
     prediction.setModelTypeAsInceptionV3()
-
-    # Directly give the path where the model is stored or use above code to join paths
-    prediction.setModelPath("server\src\inception_v3_google.pth")
+    model_path = os.path.join("server", "src", "inception_v3_google.pth")
+    prediction.setModelPath(model_path)
     prediction.loadModel()
 
     # Give path to the image which is to be classified
-    predictions = prediction.classifyImage(img, result_count=5)
+    predictions = prediction.classifyImage(img, result_count=3)
 
     # Return the JSON response
 
     return jsonify(predictions[0])
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route("/similar", methods=["POST"])
+def similar_recommender():
+    # Get the url from the request
+    url = request.args.get('url')
+
+    # Read the file
+    img = tf.keras.preprocessing.image.load_img(
+        BytesIO(file.read()), target_size=(240, 320)
+    )
+
+    # Return the JSON response
+    return jsonify()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
